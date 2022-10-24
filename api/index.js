b71a8f202ba92da19dd6674b64f2d0d4a1629d5a
Dayron Acosta <dayroned@users.noreply.github.com>
2022-10-24 16:29:48 -0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ export default function handler(request, response) {
   if (request.body["shared_secret"] !== "710b50464c35aee9d0786faf05a170e198ff80f4bf7731030287b187d6d82cfa") {
     response.status(403).send("forbidden");
   }
-  else if (request.body["mac"] === "aa:bb:cc:dd:ee:ff") {
+  else if ((request.body["mac"] || "").startsWith("52:54:00")) {
     response.status(200).send("ensure360-demo 172.16.130.199");
   }
   else {
